82bd411d3ec337c5296098f0f09f4061b73a2542
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-01 22:07:01 +0200

    used axios to fetch data from api

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const apiUri = "https://www.world-wonders-api.org/v0/wonders"
+const cardsContainer = document.getElementById("cards-container");
+const apiUri = "https://www.world-wonders-api.org/v0/wonders";
+let wondersData;
+axios.get(apiUri).then((response)=>{
+    wondersData = response.data;
+})
