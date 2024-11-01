b1bcf3cd05715b9b97b84377be8a67c7400b0b6d
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-01 22:08:08 +0200

    iterated through result object

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ const apiUri = "https://www.world-wonders-api.org/v0/wonders";
 let wondersData;
 axios.get(apiUri).then((response)=>{
     wondersData = response.data;
+    for (wonder in wondersData){
+        console.log(wondersData[wonder])
+    }
 })
