7b55a233be4ef0f2f5348bc269162d1fa0bc4277
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-01 22:26:52 +0200

    displayed a single data that is outside the array

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let wondersData;
 axios.get(apiUri).then((response)=>{
     wondersData = response.data;
     for (wonder in wondersData){
-        console.log(wondersData[wonder])
+        console.log(wondersData)
         cardsContainer.innerHTML+=`
             <div class="wonder-card">
                 <div class="wonder-image">
@@ -32,6 +32,33 @@ axios.get(apiUri).then((response)=>{
             </div>
         `
     }
+    // there is an extra data outside the array so display it separately
+    cardsContainer.innerHTML+=`
+            <div class="wonder-card">
+                <div class="wonder-image">
+                    <img
+                        src=${wondersData.links.images[0]}
+                        alt=${wondersData.name}
+                    />
+                </div>
+                <h3>${wondersData.name}</h3>
+                <p>
+                    ${wondersData.summary}
+                </p>
+                <em>${wondersData.location}</em>
+                <p>${wondersData.build_year < 0? `${Math.abs(wondersData.build_year)} B.C`: `${wondersData.build_year} A.C`}</p>
+                <em>${wondersData.time_period}</em>
+                <div class="sources-container">
+                <h4>More Info</h4>
+                <div class="sources-card">
+                    <a href=${wondersData.links.wiki} target="_blank">Wiki</a>
+                    <a href=${wondersData.links.britannica} target="_blank">Britannica</a>
+                    <a href=${wondersData.links.google_maps} target="_blank">Google Maps</a>
+                    <a href=${wondersData.links.trip_advisor} target="_blank">Trip Advisor</a>
+                </div>
+                </div>
+            </div>
+        `
 })
 
 
